d819954d1eb22b754f3de039f44717e540f9e5ff
Ghada <stare.ghada@gmail.com>
2024-03-07 23:57:19 +0100

    The principle files

---
+++ PhotoAlbum/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+
+      <div>
+        <Button variant="contained" color="primary">
+          Hello Material-UI
+        </Button>
+        <Button variant="contained">Hello world</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
